d8ec3d9fd8fad5de45a320ebca0af50524bbcb17
rjimaw7 <rjimaw7777@gmail.com>
2024-02-28 18:12:18 +0800

    added tanstack query for vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
-createApp(App).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(VueQueryPlugin);
+
+app.mount('#app');
